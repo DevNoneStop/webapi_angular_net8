184a84936219bb4e8c6c6d81e7faec9e2b0993de
DevNoneStop <Devnonestop@gmail.com>
2024-10-20 11:18:20 +0200

    PRJ04: added SqliteDB and created migration

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94261af788383690e6e5a4dc23ebbd1b88964da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7422a716b1428f53054e476d0fe1d871ea55e003")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
